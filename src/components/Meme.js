12ccd7bf88fc6383ad2615655db6f554fb343d07
ilgharr <ilgharrahno2.0@gmail.com>
2024-07-15 14:20:26 -0400

    image generator added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,22 +1,63 @@
 import React from 'react';
+import memesData from '../memesData.js'
 
 export default function Meme() {
-    return(
-        <main>
-            <form className="form">
-                <div>
-                    <label> Top Text
-                        <input type="text" className="form--inputs" placeholder="Top text"/>
-                    </label>
-                </div>
-                <div>
-                    <label> Bottom Text
-                        <input type="text" className="form--inputs" placeholder="Bottom text"/>
-                    </label>
-                </div>
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
 
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
+    // const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"])
+    //
+    // function addItem() {
+    //     setThingsArray(prevThingsArray => {
+    //         return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`]
+    //     })
+    // }
+//###########################################################
+    // const [contact, setContact] = React.useState({
+    //     firstName: "John",
+    //     lastName: "Doe",
+    //     phone: "+1 (719) 555-1212",
+    //     email: "itsmyrealname@example.com",
+    //     isFavorite: false
+    // })
+    //
+    // let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+    //
+    // function toggleFavorite() {
+    //     setContact(prevContact => ({
+    //         ...prevContact,
+    //         isFavorite: !prevContact.isFavorite
+    //     }))
+    // }
+//###########################################################
+
+    return (
+        <main>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={memeImage} className="meme--image"/>
         </main>
     )
 }
